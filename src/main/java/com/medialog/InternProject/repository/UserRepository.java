fc1c339b05dadea3edbbeab7176081f98d933d1c
Seongho Moon <nowstart1468@medialog.co.kr>
2021-08-11 23:36:57 +0900

    selectOne commit

---
+++ src/main/java/com/medialog/InternProject/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.medialog.InternProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medialog.InternProject.model.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+
+}
