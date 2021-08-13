13f265eb4796d4bfd109686153a822c3f7e010e2
KR620574 <leesrock113@gmail.com>
2021-08-13 09:41:50 +0900

    로거 하나 빠트림

--- src/main/java/com/medialog/InternProject/security/WebMvcConfig.java
+++ src/main/java/com/medialog/InternProject/security/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package com.medialog.InternProject.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -9,12 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private Logger logger = LoggerFactory.getLogger(BearerAuthInterceptor.class);
+
     @Autowired
     private HandlerInterceptor bearerAuthInterceptor;
 
 
     public void addInterceptors(InterceptorRegistry registry){
-        System.out.println(">>> Register Interceptor");
+        logger.info(">>> Register Interceptor");
         registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/test/user");
     }
 }
