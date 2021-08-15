58db9f7957dfaef29333f7983bab29eccb3afce7
KR620574 <leesrock113@gmail.com>
2021-08-15 11:22:52 +0900

    node.ks, express 서버 설정됨

--- front/app.js
+++ front/app.js
@@ -5,9 +5,8 @@ app.listen(3000,function() {
 });
 
 
-// localhost:3000/main 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
 app.get('/', function(req,res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
-app.use(express.static(''))
+app.use(express.static('public'));
