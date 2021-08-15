02068c0acd5139a055c7773d1bce45578e7328ce
KR620574 <leesrock113@gmail.com>
2021-08-16 00:54:06 +0900

    1. user type select 2. 안쓰는 파일 제거

--- front/public/js/register.js
+++ front/public/js/register.js
@@ -3,7 +3,6 @@ const buttonCancel = document.querySelector("footer").querySelector(".btn#cancel
 const userForm = document.querySelector("form");
 const passwordCheckInput = userForm.getElementsByTagName("input").passwordCheck;
 const passwordInput = userForm.getElementsByTagName("input").password;
-const userTypeCheck = document.getElementById("userTypeCheck");
 
 let isUniqueID = true;
 let isPasswordChecked = false;
@@ -16,7 +15,6 @@ buttonCancel.addEventListener("click",handleButtonCancel);
 passwordCheckInput.addEventListener("change",checkPassword);
 passwordInput.addEventListener("change",checkPassword);
 
-userTypeCheck.addEventListener('click',checkUserTypeHandler);
 
 function handleButtonSubmit(event){
     event.preventDefault();
@@ -29,9 +27,10 @@ function handleButtonSubmit(event){
         5. 가입이 완료 된 후에는 주소 이외의 정보 수정을 금지합니다.
      */
     try {
-        //checkUniqueID();
-        hasCheckedPasswordStatus();
         checkInput();
+        checkUniqueID();
+        hasCheckedPasswordStatus();
+        checkUniqueID();
         requestUserRegister();
         unableSubmitForm();
     } catch (error) {
@@ -40,17 +39,20 @@ function handleButtonSubmit(event){
 };
 
 function checkUniqueID(){
-    if(!isUniqueID){
+    const able="사용 가능한 id입니다.";
+    const idCheckRes = document.getElementById("idCheckRes");
+
+    if(idCheckRes.value != albe){
         alert("아이디 중복검사 해주세요.");
         throw("아이디 중복");
     }
-};
+}
 
 // 이미 패스워드 체크가 되어있는지 검사합니다.
 function hasCheckedPasswordStatus(){
         if(!isPasswordChecked){
-        alert("패스워드를 확인해 주세요.");
-        throw("패스워드 불일치");
+            alert("패스워드를 확인해 주세요.");
+            throw("패스워드 불일치");
         }
 };
 
@@ -123,14 +125,12 @@ function checkPassword(){
 };
 
 
-function checkUserTypeHandler(){
-    const userTypeRadios = document.getElementsByTagName("userTypeCheck");
-    const registerNumber = document.getElementsByName("regNo");
-    if(userTypeRadios[0].getAttribute("checked")){
-        registerNumber[0].classList.remove('hide');
-    }else if(userTypeRadios[1].getAttribute("checked")){
-        registerNumber[0].classList.add('hide');
-    }
 
-    console.log(isPasswordChecked);
-};
+function selectUserType(value){
+    const inputRegNo =   document.getElementById('regNoInput')
+    if(value == 'agent'){
+        inputRegNo.classList.remove('hide');
+    }else{
+        inputRegNo.classList.add('hide');
+    }
+}
