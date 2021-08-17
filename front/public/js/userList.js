c2b1f0814e96bc78bf86f18bb6b237dd1b0514a9
HyeRim-Choi <hyerim252@gmail.com>
2021-08-17 16:28:39 +0900

    userList.js 수정

--- front/public/js/userList.js
+++ front/public/js/userList.js
@@ -16,35 +16,37 @@ fetch(url,
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data.result);
+        
         if (data.message == "Empty List"){
             notExistUser();
         }
 
-       for (var i = 0; i < data.result.length; i++) { 	
+        else if(data.message == "Success"){
+            for (var i = 0; i < data.result.length; i++) { 	
 
-            const addr = JSON.parse(data.result[i].addr);
-
-            const user = {
-                name : data.result[i].name,
-                id : data.result[i].loginId,
-                city : addr.city,
-                country : addr.country,
-                dong : addr.dong  
+                const addr = JSON.parse(data.result[i].addr);
+    
+                const user = {
+                    name : data.result[i].name,
+                    id : data.result[i].loginId,
+                    city : addr.city,
+                    country : addr.country,
+                    dong : addr.dong  
+                }
+    
+                userList.push(user);
             }
-
-            userList.push(user);
-        }
-
-
-        let t_body = document.getElementById("t_body");
-
-        for(let i = 0; i < userList.length; i++){
-            t_body.innerHTML += "<tr> <th scope='row'>" + 
-                                    (i + 1) +"</th> <td>" + 
-                                    userList[i].name + "</td> <td>" + 
-                                    userList[i].id + "</td> <td>" + 
-                                    userList[i].city + " " + userList[i].country + " " + userList[i].dong + "</td> </tr>";
+    
+    
+            let t_body = document.getElementById("t_body");
+    
+            for(let i = 0; i < userList.length; i++){
+                t_body.innerHTML += "<tr> <th scope='row'>" + 
+                                        (i + 1) +"</th> <td>" + 
+                                        userList[i].name + "</td> <td>" + 
+                                        userList[i].id + "</td> <td>" + 
+                                        userList[i].city + " " + userList[i].country + " " + userList[i].dong + "</td> </tr>";
+            }
         }
     })
     .catch(err => {
